920545dd2974695fbc7e5646c184b30142249d9c
ddbbc <sd-baby@foxmail.com>
2019-04-15 15:17:21 +0800

    Add base template and error template

--- app.py
+++ app.py
@@ -20,9 +20,8 @@ db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
-	user = User.query.first()
 	movies = Movie.query.all()
-	return render_template('index.html', user=user, movies=movies)
+	return render_template('index.html', movies=movies)
 
 @app.route('/user/<name>')
 def user_page(name):
@@ -36,6 +35,15 @@ def test_url_for():
         print(url_for('test_url_for'))
         print(url_for('test_url_for', num=2))
         return 'Test page %s' % url_for('test_url_for', num=2)
+
+@app.context_processor
+def inject_user():
+	user = User.query.first()
+	return dict(user=user)
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'), 404
 
 class User(db.Model):
         id = db.Column(db.Integer, primary_key = True)
